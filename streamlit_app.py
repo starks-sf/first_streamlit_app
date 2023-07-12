53524875a69541c59b9187c3c17f11a52d717033
starks-sf <139336232+starks-sf@users.noreply.github.com>
2023-07-12 15:17:02 +0530

    Update streamlit_app.py
    fixing issues

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-
 stramlit.header('Breakfast Menu')
 stramlit.text('Omega 3 & Blueberry Oateal')
 stramlit.text('Kale, Spinach & Rocket Smoothie')
