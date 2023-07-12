c121356970df29d653b4e75a0c5e0f9863e7f77d
starks-sf <139336232+starks-sf@users.noreply.github.com>
2023-07-12 15:15:50 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My Parents New Healthy Diner')
 stramlit.header('Breakfast Menu')
 stramlit.text('Omega 3 & Blueberry Oateal')
 stramlit.text('Kale, Spinach & Rocket Smoothie')
-stramlit.text('Hard-Boiled Free-Range Egg'
+stramlit.text('Hard-Boiled Free-Range Egg')
   
