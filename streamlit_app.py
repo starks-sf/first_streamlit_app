7fa2305f6268606bdc326315d7cd404efd9d10c1
starks-sf <139336232+starks-sf@users.noreply.github.com>
2023-07-14 20:41:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,28 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+      if not fruit_choice:
+            streamlit.error("Please select a fruit to get information.")
+      else:
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+            #normalize the json
+            fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+            #populate the json in tabular view
+            streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+streamlit.error()
+            
 streamlit.write('The user entered ', fruit_choice)
 
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #normalize the json
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #populate the json in tabular view
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
