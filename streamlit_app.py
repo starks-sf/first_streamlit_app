8964c5bcc8c9ca9dfcc80cf24210124a1dd685fa
starks-sf <139336232+starks-sf@users.noreply.github.com>
2023-07-14 19:38:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,5 +40,6 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load data contains:")
-streamlit.dataframe(my_data_rows)
 
+fruit_list=streamlit.dataframe(my_data_rows)
+add_my_fruit=fruit_list.set_index('jackfruit')
