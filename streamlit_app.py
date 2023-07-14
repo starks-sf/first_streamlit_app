b67904a1c3b3559899be488a20531445b94f2b5f
starks-sf <139336232+starks-sf@users.noreply.github.com>
2023-07-14 21:21:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,17 +44,6 @@ try:
 except URLError as e:
       streamlit.error()
             
-streamlit.write('The user entered ', fruit_choice)
-
-
-
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#normalize the json
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#populate the json in tabular view
-#streamlit.dataframe(fruityvice_normalized)
-
-#streamlit.stop()
 
 streamlit.header("The fruit load data contains:")
 #snowflake
@@ -69,18 +58,6 @@ if streamlit.button('Get fruit load list'):
       my_data_rows=get_fruit_load_list()
       streamlit.dataframe(my_data_rows)
       
-      
-
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-#my_data_rows = my_cur.fetchall()
-#streamlit.header("The fruit load data contains:")
-#mylist=streamlit.dataframe(my_data_rows)
-
-#add_my_fruit = streamlit.text_input('What would you like to add:') 
-#streamlit.write('Thanks for adding ', add_my_fruit)
-#my_cur.execute("insert into fruit_load_list values('from streamlit')")
 
 
 
