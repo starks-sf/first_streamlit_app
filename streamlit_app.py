1ad020a664a618300a9853187e09823dca6096b1
starks-sf <139336232+starks-sf@users.noreply.github.com>
2023-07-14 20:28:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ mylist=streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What would you like to add:') 
 streamlit.write('Thanks for adding ', add_my_fruit)
-my_cur.execute("insert into fruit_load_values('from streamlit')")
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
 
 
 
