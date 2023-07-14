c72e1335ae160d4e605c3e11ccb32c58e7689047
starks-sf <139336232+starks-sf@users.noreply.github.com>
2023-07-14 20:35:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 import snowflake.connector
 from urllib.error import URLError
 
@@ -29,7 +30,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #normalize the json
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
